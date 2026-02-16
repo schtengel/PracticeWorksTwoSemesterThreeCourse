7745c87ede84ce597bff79452a2fabf76cb6a558
katzilipsiz <katzilipsiz@gmail.com>
2026-02-16 17:59:47 +0500

    9

--- PR3/Program.cs
+++ PR3/Program.cs
@@ -1,3 +1,72 @@
-﻿/*Фабрика животных
-Создайте фабричный метод для порождения различных типов животных (Dog, Cat, Bird).*/
+﻿/*Создание документов
+Реализуйте фабричный метод для генерации документов (PDFDocument, WordDocument, TextDocument).*/
 
+interface IDocument
+{
+    void Info();
+}
+
+class Pdf : IDocument
+{
+    public void Info() => Console.WriteLine("PDF-Документ");
+}
+
+class Word : IDocument
+{
+    public void Info() => Console.WriteLine("Word-Документ");
+}
+
+class Text : IDocument
+{
+    public void Info() => Console.WriteLine("TXT-Документ");
+}
+
+abstract class DocumentFactory
+{
+    public abstract IDocument FactoryMethod();
+
+    public void InfoGoods()
+    {
+        IDocument document = FactoryMethod();
+        document.Info();
+    }
+}
+
+class PdfFactory : DocumentFactory
+{
+    public override IDocument FactoryMethod()
+    {
+        return new Pdf();
+    }
+}
+
+class WordFactory : DocumentFactory
+{
+    public override IDocument FactoryMethod()
+    {
+        return new Word();
+    }
+}
+
+class TextFactory : DocumentFactory
+{
+    public override IDocument FactoryMethod()
+    {
+        return new Text();
+    }
+}
+
+class Client
+{
+    public static void Main()
+    {
+        DocumentFactory pdfFactory = new PdfFactory();
+        DocumentFactory wordFactory = new WordFactory();
+        DocumentFactory textFactory = new TextFactory();
+
+        pdfFactory.InfoGoods();
+        wordFactory.InfoGoods();
+        textFactory.InfoGoods();
+
+    }
+}
