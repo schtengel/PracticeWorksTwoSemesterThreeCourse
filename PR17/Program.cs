f0786e2733dd71f190113dcc4e100fa8cf01b708
katzilipsiz <katzilipsiz@gmail.com>
2026-04-22 10:39:20 +0500

    олщ

---
+++ PR17/Program.cs
@@ -0,0 +1,74 @@
+﻿/*Банкомат (ATM)
+Создайте IATMState, NoCardState, HasCardState, HasPinState, NoCashState, ATM, чтобы управлять логикой ввода карты, ввода PIN-кода и снятия денег.*/
+
+public interface IATMState
+{
+    void Handle(ATMContext context);
+}
+
+public class NoCardState : IATMState
+{
+    public void Handle(ATMContext context)
+    {
+        Console.WriteLine("Имеется ли у вас карта?");
+        context.SetState(new HasCardState());
+    }
+}
+
+public class HasCardState : IATMState
+{
+    public void Handle(ATMContext context)
+    {
+        Console.WriteLine("Пинкод карты: ");
+        context.SetState(new HasPinState());
+    }
+}
+
+public class HasPinState : IATMState
+{
+    public void Handle(ATMContext context)
+    {
+        Console.WriteLine("На карте недостаточно средств.");
+        context.SetState(new NoCashState());
+    }
+}
+
+public class NoCashState : IATMState
+{
+    public void Handle(ATMContext context)
+    {
+        Console.WriteLine("На карте недостаточно средств.");
+    }
+}
+
+public class ATMContext
+{
+    private IATMState _state;
+
+    public ATMContext()
+    {
+        _state = new NoCardState();
+    }
+
+    public void SetState(IATMState state)
+    {
+        this._state = state;
+    }
+
+    public void NextStep()
+    {
+        _state.Handle(this);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ATMContext atm = new ATMContext();
+
+        atm.NextStep();
+        atm.NextStep();
+        atm.NextStep();
+    }
+}
---
+++ PR18/Program.cs
@@ -0,0 +1,85 @@
+﻿/*Способы оплаты в интернет-магазине
+Создайте IPaymentStrategy, CreditCardPayment, PayPalPayment, BitcoinPayment, PaymentProcessor, чтобы пользователь мог выбрать способ оплаты.*/
+
+public interface IPaymentStrategy
+{
+    void Pay(double amount);    
+}
+
+public class CreditCardPayment : IPaymentStrategy
+{
+    public void Pay(double amount) 
+    {
+        Console.WriteLine($"Оплата с использованием банковской карты {amount}");
+    }
+}
+public class PayPalPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Оплата с использованием PayPal {amount}");
+    }
+}
+
+public class BitcoinPayment : IPaymentStrategy
+{
+    public void Pay(double amount)
+    {
+        Console.WriteLine($"Оплата с использованием биткоина {amount}");
+    }
+}
+
+public class PaymentProcessor
+{
+    private IPaymentStrategy _strategy;
+
+    public void SetPaymentStrategy(IPaymentStrategy strategy )
+    {
+        _strategy = strategy;
+    }
+
+    public void ExecutePayment(double amount)
+    {
+        if( _strategy == null )
+        {
+            Console.WriteLine("Ошибка: метод оплаты не выбран!");
+            return;
+        }
+
+        _strategy.Pay(amount);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        PaymentProcessor context = new PaymentProcessor();
+
+        Console.WriteLine("Выберите метод оплаты:");
+        Console.WriteLine("1 - Банковская карта");
+        Console.WriteLine("2 - PayPal");
+        Console.WriteLine("3 - Биткоин");
+
+        string choice = Console.ReadLine();
+        double amount = 500.0; // сумма к оплате
+
+        switch (choice)
+        {
+            case "1":
+                context.SetPaymentStrategy(new CreditCardPayment());
+                break;
+            case "2":
+                context.SetPaymentStrategy(new PayPalPayment());
+                break;
+            case "3":
+                context.SetPaymentStrategy(new BitcoinPayment());
+                break;
+            default:
+                Console.WriteLine("Ошибка: некорректный выбор.");
+                return;
+        }
+
+        context.ExecutePayment(amount);
+    }
+}
